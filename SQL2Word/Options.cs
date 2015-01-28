b797a24f116c2c2a7c9c44358ed8dcda2aba6a21
mick <mmailm@mail.ru>
2015-01-29 02:00:57 +0800

    Добавил чтение credentials и передача опций

---
+++ SQL2Word/Options.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using CommandLine;
+using CommandLine.Text;
+
+namespace SQL2Word
+{
+    class Options
+    {
+
+        [Option('i', "input_file", Required = true, HelpText = "docx template file")]
+        public string TemplateFile { get; set; }
+        [Option('o', "output_file", HelpText = "File to which output goes")]
+        public string OutputFile { get; set; }
+
+        [Option('a', "append", HelpText = "append output to existing files in output directory",
+            DefaultValue = false)]
+        public bool Append { get; set; }
+
+        [OptionArray('p', "params",
+            HelpText = "pass parameters as list of tuples: key1=value1 key2=value2 ...")]
+        public string[] ParametersArg { get; set; }
+
+        [Option('s', "show parameters", HelpText = "show available parameters")]
+        public bool ListParameters { get; set; }
+
+        [Option('c', "connection options", HelpText = "file with connection options:"
+            + "\n\tUserID=YourUsername"
+            + "\n\tPassword=YourPass"
+            + "\n\tDataSource=YourDataSourceName"
+            + "\n\tInitialCatalog=YourInitialCatalogName")]
+        public string CredentialFile { get; set; }
+
+        public Dictionary<string, string> Parameters
+        {
+            get
+            {
+                var reg = new Regex(@"(\w+)=(.+)");
+                var output = new Dictionary<string, string>();
+                if (ParametersArg != null)
+                {
+                    foreach (var item in ParametersArg)
+                    {
+                        var m = reg.Match(item);
+                        if (m.Success)
+                        {
+                            output.Add(m.Groups[1].Value, m.Groups[2].Value);
+                        }
+                    }
+                }
+                return output;
+            }
+        }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+                (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ SQL2Word/Script.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SQLDynamic
+{
+    public class Script
+    {
+        String _text;
+        String _title;
+        Regex reg = new Regex(@"/\*\$(\w+)\*/(.*?)/\*\$\*/");
+        Dictionary<string, string> prms = new Dictionary<string, string>();
+
+        public Script(String title, FileInfo file)
+        {
+            using(var s = new StreamReader(file.OpenRead(), Encoding.Default)) {
+                String text = s.ReadToEnd();
+                Init(title, text);
+            }
+        }
+
+        public Script(String title, String text)
+        {
+            Init(title, text);
+        }
+
+        public void Init(String title, String text)
+        {
+            this._text = text;
+            this._title = title;
+            foreach(Match m in reg.Matches(text)) {
+                prms.Add(m.Groups[1].Value, m.Groups[2].Value);
+            }
+        }
+
+        public String Title
+        {
+            get { return _title; }
+        }
+
+        public Dictionary<string, string> Params
+        {
+            get
+            {
+                return prms;
+            }
+
+        }
+
+        public String Text(Dictionary<String, String> prms)
+        {
+            var evaluator = new MatchEvaluator(new Func<Match, String>( m =>
+            {
+                if (prms.ContainsKey(m.Groups[1].Value))
+                {
+                    return prms[m.Groups[1].Value];
+                }
+                return m.Value;
+            }));
+            return reg.Replace((String)this._text.Clone(), evaluator);
+        }
+    }
+}
