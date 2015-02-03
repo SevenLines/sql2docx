ad3e980a8d074afba4fb09d544b7d8e47580148b
mick <mmailm@mail.ru>
2015-02-04 04:26:36 +0800

    Добавил файлы из игнора

---
+++ SQL2Word/Filler.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Novacode;
+using Script = SQLDynamic.Script;
+
+namespace SQL2Word
+{
+    class Filler
+    {
+        public enum TOKENS
+        {
+            CONTENT,
+            WITH_COUNTER,
+            USE_ZERO_CONUTER,
+            AUTO_EXPAND,
+            REPLACE_TABLE_ON_EMPTY,
+            UPDATE_SCRIPT,
+        };
+
+        static Regex re_REPLACE_TABLE_ON_EMPTY = new Regex(@"\[REPLACE_TABLE_ON_EMPTY(:.*?)?\]");
+        static Regex re_UPDATE_SCRIPT = new Regex(@"\[UPDATE_SCRIPT(:(.*?))?\]");
+
+        private static String wordopenxml_namespace = "http://schemas.openxmlformats.org/wordprocessingml/2006/main";
+
+        /// <summary>
+        /// Return all tokens should used in current cell,
+        /// All tokens should be in first paragraph
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <returns></returns>
+        public static List<TOKENS> GetCellTokens(Cell cell, out Dictionary<TOKENS, String> special_parameters)
+        {
+            var output = new List<TOKENS>();
+            special_parameters = new Dictionary<TOKENS, string>();
+
+            Match match;
+
+            special_parameters = new Dictionary<TOKENS, string>();
+            var text = cell.Paragraphs.First().Text;
+            if (text.Contains("[CONTENT]"))
+            {
+                output.Add(TOKENS.CONTENT);
+//                special_parameters[TOKENS.CONTENT] = "";
+            }
+            if (text.Contains("[WITH_COUNTER]")) { 
+                output.Add(TOKENS.WITH_COUNTER);
+                special_parameters[TOKENS.WITH_COUNTER] = "";
+            }
+            if (text.Contains("[USE_ZERO_CONUTER]")) { 
+                output.Add(TOKENS.USE_ZERO_CONUTER);
+                special_parameters[TOKENS.USE_ZERO_CONUTER] = "";
+            }
+            if (text.Contains("[AUTO_EXPAND]")) { 
+                output.Add(TOKENS.AUTO_EXPAND);
+                special_parameters[TOKENS.AUTO_EXPAND] = "";
+            }
+
+            match = re_REPLACE_TABLE_ON_EMPTY.Match(text);
+            if (match.Success)
+            {
+                output.Add(TOKENS.REPLACE_TABLE_ON_EMPTY);
+                special_parameters[TOKENS.REPLACE_TABLE_ON_EMPTY] = match.Groups[1].Value;
+            }
+
+            match = re_UPDATE_SCRIPT.Match(text);
+            if (match.Success)
+            {
+                output.Add(TOKENS.UPDATE_SCRIPT);
+                special_parameters[TOKENS.UPDATE_SCRIPT] = match.Groups[2].Value;
+            }
+
+            return output;
+        }
+
+
+        public static List<TOKENS> GetTableTokens(Table table, out Dictionary<TOKENS, string> parameters)
+        {
+            var row = GetTableScriptRow(table);
+            var cell = row.Cells.First();
+            return GetCellTokens(cell, out parameters);
+        }
+
+        public static String GetTableScript(Table table)
+        {
+            var row = GetTableScriptRow(table);
+            var cell = row.Cells.First();
+            return GetCellContent(cell);
+        }
+
+        public static Row GetTableScriptRow(Table table)
+        {
+            return table.Rows.Last();
+        }
+
+        public static void SetTableScriptRowText(Table table, String text)
+        {
+            var cell = GetTableScriptRow(table).Cells.First();
+            if (cell != null)
+            {
+                SetCellContent(cell, text);
+            }
+        }
+
+        /// <summary>
+        /// returns cell text content as all joined paragraphs text within cell
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <returns></returns>
+        public static String GetCellContent(Cell cell, int skip=1)
+        {
+            StringBuilder output = new StringBuilder();
+            foreach (var paragraph in cell.Paragraphs.Skip(skip))
+            {
+                output.AppendLine(paragraph.Text);
+            }
+            return output.ToString();
+        }
+
+        public static void SetCellContent(Cell cell, String text)
+        {
+            cell.Paragraphs.ForEach(paragraph => paragraph.Remove(false));
+            cell.InsertParagraph(text);
+        }
+
+
+        private static int _fillTable(
+            Table table, 
+            SqlConnection connection, 
+            String script,
+            List<TOKENS> tokens,
+            bool useFirstRow = false)
+        {
+            if (String.IsNullOrEmpty(script))
+            {
+                return 0;
+            }
+
+            var sql = new SqlCommand(script, connection);
+            SqlDataReader reader;
+            try
+            {
+                reader = sql.ExecuteReader();
+
+            }
+            catch (SqlException ex)
+            {
+                SetTableScriptRowText(table, ex.Message);
+                return 0;
+            }
+
+            int counter = 1;
+            int iOffset = 0;
+
+            if (tokens.Contains(TOKENS.USE_ZERO_CONUTER))
+                counter = 0;
+            if (tokens.Contains(TOKENS.WITH_COUNTER))
+                iOffset++;
+
+            bool isFirstRow = true;
+
+            do
+            {
+                while (reader.Read())
+                {
+                    Row r;
+                    if (useFirstRow && isFirstRow)
+                    {
+                        r = table.Rows.Last();
+                        r.Paragraphs.ForEach( p => p.Remove(false));
+                    }
+                    else
+                    {
+                        r = table.InsertRow();
+                    }
+
+                    if (tokens.Contains(TOKENS.WITH_COUNTER))
+                    {
+                        var p = r.Cells[0].Paragraphs.First();
+                        if (p != null)
+                            p.Append(counter.ToString());
+                    }
+
+                    if (tokens.Contains(TOKENS.AUTO_EXPAND))
+                    {
+                        while (r.ColumnCount < reader.FieldCount + iOffset)
+                        {
+                            table.InsertColumn(table.ColumnCount - 1);
+                        }
+                    }
+
+                    var maxValue = Math.Min(reader.FieldCount + iOffset, r.ColumnCount);
+                    for (int i = iOffset; i < maxValue; i++)
+                    {
+                        var p = r.Cells[i].Paragraphs.First();
+                        if (p != null)
+                            p.Append(reader.GetSqlValue(i - iOffset).ToString());
+                    }
+
+                    if (isFirstRow)
+                    {
+                        isFirstRow = false;
+                    }
+
+                    counter++;
+                }
+            } while (reader.NextResult());
+
+            reader.Close();
+
+            return counter;
+        }
+
+        private static void _addScriptRow(Table table, 
+            String script, 
+            int contentStart, 
+            Dictionary<TOKENS, string> specialParameters)
+        {
+            script = Regex.Replace(script, "--.*", "").Replace("\n", ""); // remove oneline SQL-comments
+            var tokenString = getTokensString(specialParameters);
+            if (!specialParameters.ContainsKey(TOKENS.UPDATE_SCRIPT))
+            {
+                tokenString = "[UPDATE_SCRIPT:" + contentStart.ToString() + "]" + tokenString;
+            }
+            script = "/*" + tokenString + "*/" + script;
+
+            var row = table.InsertRow();
+            row.MergeCells(0, row.ColumnCount - 1);
+            row.Height = 1;
+
+            // скрипт вставляем в последнюю ячейку с невидимым текстом, без полей и высотой 1 чего-то там
+            var cell = row.Cells.FirstOrDefault();
+            if (cell != null)
+            {
+                var border = new Border { Tcbs = BorderStyle.Tcbs_none };
+                cell.SetBorder(TableCellBorderType.Bottom, border);
+                cell.SetBorder(TableCellBorderType.Left, border);
+                cell.SetBorder(TableCellBorderType.Right, border);
+            }
+
+            row.Paragraphs.ForEach(paragraph => paragraph.Remove(false));
+            var p2 = row.Paragraphs.FirstOrDefault();
+            p2.Append(script);
+            p2.Hide();
+        }
+
+        /// <summary>
+        /// Fill table with script result, returns true on success, false otherwise
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="connection"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static bool FillTable(
+            Table table, SqlConnection connection, 
+            Dictionary<string, string> parameters,
+            bool saveQueries)
+        {
+            Dictionary<TOKENS, string> specialParameters;
+            var tokens = GetTableTokens(table, out specialParameters);
+            if (tokens.Count <= 0)
+            {
+                return false;
+            }
+
+            var dynamicScript = new Script("", GetTableScript(table));
+            var script = dynamicScript.Text(parameters);
+
+            var contentRow = GetTableScriptRow(table);
+            var contentStart = table.RowCount;
+            var counter = _fillTable(table, connection, script, tokens);
+
+            // if no values to output
+            var min = tokens.Contains(TOKENS.USE_ZERO_CONUTER) ? 0 : 1;
+            if (counter <= min)
+            {
+                if (tokens.Contains(TOKENS.REPLACE_TABLE_ON_EMPTY))
+                {
+                    table.InsertParagraphBeforeSelf(specialParameters.Default(
+                        TOKENS.REPLACE_TABLE_ON_EMPTY, ""));
+                    table.Remove();
+                }
+            }
+
+            if (saveQueries)
+            {
+                _addScriptRow(table, script, contentStart, specialParameters);
+            }
+
+            // удаляем строку со скриптом
+            contentRow.Remove();
+            
+            return true;
+        }
+
+        private static String getTokensString(Dictionary<TOKENS, string> specail_parameters)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var parameter in specail_parameters)
+            {
+                stringBuilder.Append("[");
+                stringBuilder.Append(parameter.Key);
+                if (String.IsNullOrEmpty(parameter.Value))
+                {
+                    stringBuilder.Append(parameter.Value);
+                }
+                stringBuilder.Append("]");
+            }
+            return stringBuilder.ToString();
+        }
+
+        public static void UpdateTable(
+            Table table, SqlConnection connection,
+            Dictionary<string, string> parameters,
+            bool saveQueries)
+        {
+            // скрипт должен быть в последней строке
+            var row = table.Rows.Last();
+            var script = GetCellContent(row.Cells.FirstOrDefault(), 0);
+
+            Dictionary<TOKENS, string> specailParameters;
+            var tokens = GetCellTokens(row.Cells.FirstOrDefault(), out specailParameters);
+
+            if (!tokens.Contains(TOKENS.UPDATE_SCRIPT))
+                return;
+
+            int firstDataRow;
+            if (!Int32.TryParse(specailParameters[TOKENS.UPDATE_SCRIPT], out firstDataRow))
+                return;
+
+            while (table.RowCount > firstDataRow)
+            {
+                table.Rows[firstDataRow].Remove();
+            }
+
+            script = re_UPDATE_SCRIPT.Replace(script, "");
+            var dynamicScript = new Script("", script);
+            script = dynamicScript.Text(parameters);
+
+            var contentStart = table.RowCount;
+
+            _fillTable(table, connection, script, tokens, true);
+
+            if (saveQueries)
+            {
+                _addScriptRow(table, script, contentStart, specailParameters);
+            }
+        }
+    }
+}
---
+++ SQL2Word/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using CommandLine;
+using Novacode;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace SQL2Word
+{
+    class Program
+    {
+
+
+
+        private static void DrawProgressBar(int complete, int maxVal, int barSize, char progressCharacter)
+        {
+            Console.CursorVisible = false;
+            int left = Console.CursorLeft;
+            decimal perc = (decimal)complete / (decimal)maxVal;
+            int chars = (int)Math.Floor(perc / ((decimal)1 / (decimal)barSize));
+            string p1 = String.Empty, p2 = String.Empty;
+
+            for (int i = 0; i < chars; i++) p1 += progressCharacter;
+            for (int i = 0; i < barSize - chars; i++) p2 += progressCharacter;
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(p1);
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(p2);
+
+            Console.ResetColor();
+            Console.Write(" {0}%", (perc * 100).ToString("N2"));
+            Console.CursorLeft = left;
+        }
+
+        public static SqlConnection connection = new SqlConnection();
+
+        public static KeyValuePair<string, string>? ParseOptionLine(string line)
+        {
+            var regex = new Regex(@"(\w+):\s*(\w+|[\(.)/]+)");
+            var match = regex.Match(line);
+
+            if (!match.Success) return null;
+
+            return new KeyValuePair<string, string>(match.Groups[1].Value, match.Groups[2].Value);
+        }
+
+
+        static public String GetConnectionString(String credentialFile)
+        {
+            var credentialDictionary = new Dictionary<string, string>();
+            if (File.Exists(credentialFile))
+            {
+                foreach (var line in File.ReadAllLines(credentialFile))
+                {
+                    var tuple = ParseOptionLine(line);
+                    if (!tuple.HasValue)
+                        continue;
+                    credentialDictionary.Add(tuple.Value.Key, tuple.Value.Value);
+                }
+            }
+            var connString = new SqlConnectionStringBuilder
+            {
+                ApplicationName = Assembly.GetEntryAssembly().GetName().Name,
+                DataSource = credentialDictionary.Default("DataSource", "(local)"),
+                InitialCatalog = credentialDictionary.Default("InitialCatalog", ""),
+            };
+            
+            if (credentialDictionary.ContainsKey("UserID"))
+            {
+                connString.UserID = credentialDictionary.Default("UserID", "");
+                if (credentialDictionary.ContainsKey("Password"))
+                    connString.Password = credentialDictionary.Default("Password", "");
+            }
+            else
+            {
+                connString.IntegratedSecurity = true;
+            }
+
+            return connString.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options))
+                return;
+
+            DocX doc;
+
+            try
+            {
+                doc = DocX.Load(options.TemplateFile);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return;
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return;                
+            }
+
+
+            var re = new Regex(@"{.*?}");
+            foreach (var paragraph in doc.Paragraphs)
+            {
+                if (re.IsMatch(paragraph.Text))
+                {
+                    foreach (var p in options.Parameters)
+                    {
+                        paragraph.ReplaceText("{" + p.Key + "}", p.Value);
+                    }
+                }
+            }
+
+            String credentialFile = String.IsNullOrEmpty(options.CredentialFile)
+                ? ".credentials"
+                : options.CredentialFile;
+            connection.ConnectionString = GetConnectionString(credentialFile);
+
+            Console.WriteLine("Пробую подсоединиться");
+            Console.WriteLine(connection.ConnectionString);
+            try
+            {
+                connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return;
+            }
+
+            int i = 0;
+            foreach (var t in doc.Tables)
+            {
+                DrawProgressBar(i, doc.Tables.Count, 30, '█');
+                if (options.UpdateFile)
+                {
+                    Filler.UpdateTable(t, connection, options.Parameters, options.SaveQueries);
+                }
+                else
+                {
+                    Filler.FillTable(t, connection, options.Parameters, options.SaveQueries);
+                }
+                i++;
+            }
+            DrawProgressBar(doc.Tables.Count, doc.Tables.Count, 30, '█');
+            Console.WriteLine();
+            Console.WriteLine("Сохраняю резульат в " + options.OutputFile);
+
+            try
+            {
+                doc.SaveAs(options.OutputFile);
+                if (options.ShowResultFileOnEnd)
+                {
+                    Process.Start(options.OutputFile);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+            connection.Close();
+            
+            
+        }
+    }
+}
